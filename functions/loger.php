29c55c8afbb228b3fa57fa440b1df243a1643428
eliabe <eliabepaz.work@gmail.com>
2022-02-07 17:04:45 -0300

    depto_cadastro

--- functions/loger.php
+++ functions/loger.php
@@ -28,6 +28,7 @@ if($_POST){
           $_SESSION['usuario']['login'] = $row_user['usuario'];
           $_SESSION['usuario']['nome'] = $row_user['nome'];
           $_SESSION['usuario']['tipo'] = $row_user['tipo'];
+          $_SESSION['usuario']['depto'] = $row_user['depto'];
           
           $_SESSION['msg'] = "Bem Vindo!";
           header("Location:../panel/index.php");
--- panel/index.php
+++ panel/index.php
@@ -1,50 +1,66 @@
+<?php
+session_start();
+//var_dump($_SESSION);
 
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<script src="https://kit.fontawesome.com/9104146bde.js"
-    crossorigin="anonymous"></script>
-	<link rel="stylesheet" href="styles.css">
+    <script src="https://kit.fontawesome.com/9104146bde.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="styles.css">
     <title>Início | Redepharma</title>
 </head>
+
 <body>
+
     <section class="header">
+
+
         <div class="hub">
-            <!-- box button -->
+
+            <div>
+                <h1>Sistema em Implementação</h1>
+                <strong>Aguardando Cadastro dos funcionarios do Financeiro e do Compras</strong>
+
+            </div>
+            <br>
             <div class="btn_container">
-                <div onclick="location.href='./inventario'" class="btn_box">
+                <div onclick="location.href='#'" class="btn_box" style="cursor: not-allowed;">
                     <div class="btn_icon"></div>
                     <div class="btn_name">Inventário</div>
                 </div>
-                <div onclick="location.href='./integrador_delivery'" class="btn_box" style="cursor: not-allowed;">
+                <div onclick="location.href='#'" class="btn_box" style="cursor: not-allowed;">
                     <div class="btn_icon"></div>
                     <div class="btn_name">Delivery</div>
                     <small>*desenvolvimento*</small>
                 </div>
-                <div onclick="location.href='./verbas/panel/'" class="btn_box">
+                <div onclick="location.href='+'" class="btn_box" style="cursor: not-allowed;">
                     <div class="btn_icon"></div>
                     <div class="btn_name">Verbas</div>
-					
+
                 </div>
                 <div onclick="location.href='#'" class="btn_box" style="cursor: not-allowed;">
                     <div class="btn_icon"></div>
                     <div class="btn_name">Chamados</div>
-					<small>*desenvolvimento*</small>
+                    <small>*desenvolvimento*</small>
                 </div>
                 <div onclick="location.href='#'" class="btn_box" style="cursor: not-allowed;">
                     <div class="btn_icon"></div>
                     <div class="btn_name">BI</div>
-					<small>*desenvolvimento*</small>
+                    <small>*desenvolvimento*</small>
                 </div>
-                <div onclick="location.href='../index.php'" class="btn_box">
+                <div onclick="location.href='../functions/logout.php'" class="btn_box">
                     <div class="btn_icon"></div>
                     <div class="btn_name">Sair</div>
                 </div>
             </div>
+
         </div>
     </section>
 </body>
+
 </html>
