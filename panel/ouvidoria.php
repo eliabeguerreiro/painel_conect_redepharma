4028889fcc59fd9f827bdc7ff8c2867a57c0cac3
Eliabe G da Paz <eliabepaz.work@gmail.com>
2022-07-13 16:10:05 -0300

    erro login ouvidoria

--- panel/ouvidoria.php
+++ panel/ouvidoria.php
@@ -1,4 +1,5 @@
 <?php
 session_start();
 header("Location:http://10.7.0.214/ouvidoria/index.php?login=".$_SESSION['login']."&senha=".$_SESSION['senha']);
+header("Location:http://10.7.0.214/ouvidoria/index.php?login=".$_SESSION['usuario']['login']."&senha=".$_SESSION['senham']);
 ?>
